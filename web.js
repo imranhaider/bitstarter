f765c0a076f6f64f23b02aa8ae37c5ea3056f909
Imran Haider <saasbook@saasbook.(none)>
2013-07-10 17:59:00 -0700

    Update web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fileContent = fs.readFileSync('index.html');
-  response.send(fileContent);
+var fileContent = fs.readFileSync('index.html','utf-8');
+  response.send(fileContent.toString());
 });
 
 var port = process.env.PORT || 5000;
